10a70c8ad2e714815f4f18ca60e04244b47c93f9
shipeibo <shipeibo@genomics.cn>
2024-11-13 17:58:44 +0800

    Add new feature or update

--- scripts/virus_indentification.sh
+++ scripts/virus_indentification.sh
@@ -11,7 +11,7 @@ palmscan="../bin/palmscan2"
 # Output directory
 output_dir="./scripts"
 # Tax Script directory
-tax_Script="./blastp_tax.py"
+tax_Script="./scripts/blastp_tax.py"
 # Tax Script directory
 tax_file="./virushostdb.formatted.cds_tax.txt"
 # Create output directory
@@ -72,7 +72,7 @@ ${palmscan} -search_pssms ./${seqID}/megahit/${seqID}_addname.faa \\
 
 # Step 4.1: BLASTP for Functional Annotation
 diamond blastp -q ./${seqID}/palmscan_results/${seqID}.core.fasta -d ${virushostdb}/virushostdb_protein.dmnd -o ./${seqID}/blastp_results.txt --evalue 1e-5 --top 5
-python ${tax_Script} -tax ${tax_file} -i ./${seqID}/blastp_results.txt -o ./${seqID}/blastp_results_tax.txt" > ${script_file}
+python ${tax_Script} -tax ${tax_file} -i ./${seqID}/blastp_results.txt -o ./${seqID}/blastp_results_tax.txt " > ${script_file}
 
   # Make the script executable
   chmod +x ${script_file}
